76766856599ece7fbf85f7fcf90167788e09e83a
niki03125 <Nikita.po@hotmail.com>
2024-10-26 15:40:54 +0200

    update

--- SP2/src/Author.java
+++ SP2/src/Author.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class Author {
+
+    private String name;
+    private ArrayList<Title> titles;
+
+    public Author(String name){
+        this.name = name;
+        this.titles = new ArrayList<>();
+    }
+
+    public void addTitle(Title title){
+        titles.add(title);
+    }
+
+    public float calculateTotalPay(){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public ArrayList<Title> getTitles(){
+        return titles;
+    }
+
+    public ArrayList<Title> setTitles(ArrayList<Title> titles){
+        this.titles = new ArrayList<>(titles);
+    }
+
+
 }
---
+++ SP2/src/PrintedBook.java
@@ -0,0 +1,2 @@
+public class PrintedBook {
+}
